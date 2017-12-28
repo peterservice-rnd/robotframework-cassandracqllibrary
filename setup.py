9d819a7f9508bc748f4ccff69c3ae31be4a1db47
Aleskarova, Dinara <dinara.aleskarova@simbirsoft.com>
2017-12-28 16:41:42 +0400

    Added library, setup.py, requirements and travis config

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""Setup module for Robot Framework Cassandra CQL Library package."""
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='robotframework-cassandracqllibrary',
+    version='1.0.0',
+    description='A Robot Framework Cassandra Database Library',
+    long_description=long_description,
+    url='https://github.com/peterservice-rnd/robotframework-cassandracqllibrary',
+    author='AIST',
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='testing,robot framework,cassandra,cql',
+    packages=find_packages(),
+    install_requires=['cassandra-driver', 'robotframework'],
+)
